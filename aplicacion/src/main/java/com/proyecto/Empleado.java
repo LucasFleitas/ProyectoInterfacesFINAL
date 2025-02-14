0ff6cfdf93613fdc643e0426d0b0328fb4e43d95
LucasFleitas <lucsf53@gmail.com>
2025-02-14 20:23:05 +0100

    Logica de ejemplo

---
+++ aplicacion/src/main/java/com/proyecto/Empleado.java
@@ -0,0 +1,43 @@
+package com.proyecto;
+
+import java.sql.Date;
+
+public class Empleado {
+    private int idEmpleado;
+    private String nombre;
+    private String cargo;
+    private Date fechaContratacion;
+
+    // Getters y Setters
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public Date getFechaContratacion() {
+        return fechaContratacion;
+    }
+
+    public void setFechaContratacion(Date fechaContratacion) {
+        this.fechaContratacion = fechaContratacion;
+    }
+}
---
+++ aplicacion/src/main/java/com/proyecto/Venta.java
@@ -0,0 +1,62 @@
+package com.proyecto;
+
+import java.sql.Date;
+
+public class Venta {
+    private int idVenta;
+    private int idEmpleado;
+    private int idProducto;
+    private int cantidad;
+    private Date fechaVenta;
+    private double totalVenta;
+
+    // Getters y Setters
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Date getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(Date fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public double getTotalVenta() {
+        return totalVenta;
+    }
+
+    public void setTotalVenta(double totalVenta) {
+        this.totalVenta = totalVenta;
+    }
+}
+
